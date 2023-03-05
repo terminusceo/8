319a7be1105357913c54d68863a37a23d18821a9
terminusceo <125861517+terminusceo@users.noreply.github.com>
2023-03-06 00:57:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ function App() {
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
   const [feedback, setFeedback] = useState(`Click to mint your NAOMI.`);
-  const [mintAmount, setMintAmount] = useState(20);
+  const [mintAmount, setMintAmount] = useState(1);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
     SCAN_LINK: "",
